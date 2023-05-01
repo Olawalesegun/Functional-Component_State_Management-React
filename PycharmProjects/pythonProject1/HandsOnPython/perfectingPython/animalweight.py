27a5ffe00aa5b37f3ebcaa397dfe37c49d327062
Olawalesegun <olawalesegun0@gmail.com>
2023-05-01 10:25:30 +0100

    a program to check out an animal weight in python

---
+++ PycharmProjects/pythonProject1/HandsOnPython/perfectingPython/animalweight.py
@@ -0,0 +1,9 @@
+def floatobject():
+    weight = 0.2
+    animal = "newt"
+    print(f"{weight} kg is the weight of the {animal}")
+    print("{0} kg is the weight of the {1}".format(weight, animal))
+
+floatobject()
+
+
