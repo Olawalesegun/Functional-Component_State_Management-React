d75bf8a6076756102439a76c530410d666dfe4d6
Olawalesegun <olawalesegun0@gmail.com>
2023-05-01 10:20:21 +0100

    a program to print a user's age in Python

---
+++ PycharmProjects/pythonProject1/HandsOnPython/perfectingPython/age.py
@@ -0,0 +1,34 @@
+#agePutIn = "Kindly enter your age"
+#age = input(agePutIn)
+#age = 15
+#print("I am " + age + "years old")
+sentence = "Shola is a girl"
+print(sentence)
+
+
+# rstrip, lstrip, strip
+x = "Yemi"
+print(x * 3)
+
+sentence2 = "Shola what did you add in your stew"
+
+age = 45
+sentence3 = "Folake is " + str(age) + " years old"
+print(sentence3)
+
+name = "Rotimi Nicholiiiiiiiiiiiii"
+print(name.find("Arrow"))
+
+newFind =name.replace("Nichol", "Arrow")
+
+print(newFind)
+print(name.find("Arrow"))
+
+print(newFind.find("Arrow"))
+
+ 
+new_name = name[0] + name[1] + name[2]
+print(new_name)
+
+new_name = name[1:5:2]
+print(new_name)
