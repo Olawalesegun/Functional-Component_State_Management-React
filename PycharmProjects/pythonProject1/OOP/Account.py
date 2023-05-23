0e7307260a9004e4346fcc6c32dca35c92a95b70
Olawalesegun <olawalesegun0@gmail.com>
2023-05-23 11:07:06 +0100

    OOP in python with Account

---
+++ PycharmProjects/pythonProject1/OOP/Account.py
@@ -0,0 +1,32 @@
+from decimal import Decimal
+
+
+class Account:
+    def __init__(self, name: str, balance: Decimal):
+        self.name = name
+        self.balance = balance
+
+    @property
+    def balance(self):
+        return self.__balance
+
+    @balance.setter
+    def balance(self, amount):
+        if amount < 0:
+            raise ValueError("Balance cannot be negative..")
+        self.__balance = amount
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        self.__name = name
+
+    def __str__(self):
+        return f"{self.name} {self.balance}"
+
+
+account1 = Account("mariam", Decimal(100.000))
+print(account1)
